c3cfbb518c7527d770b1621f959abe89ceeaf740
Cahlan Sharp <cahlan@gmail.com>
2013-10-05 01:35:15 -0600

    files

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+
+	function convert_value_to_string(value) {
+		if (value > 10) {
+			switch (value) {
+				case 11:
+				return 'Jack';
+				break;
+				case 12:
+				return 'Queen';
+				break;
+				case 13:
+				return 'King';
+				break;
+			}
+		}
+		return value.toString();
+	}
+
 	var deck = [];
 	var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
 	for (var i = 0; i<suits.length; i++) {
