f6937762666428a96cf2f12996ce7230880974da
Cahlan Sharp <cahlan@gmail.com>
2013-10-05 00:43:51 -0600

    initial files added

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	var deck = [];
+	var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+	for (var i = 0; i<suits.length; i++) {
+		var suit = suits[i];
+		for (var j = 0; j<13; j++) {
+			deck.push({value:j+1, suit: suit});
+		}
+	}
+});
