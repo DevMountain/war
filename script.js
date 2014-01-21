7bb370ed65d8b2d2ea31c9eb8d567e48666b53b1
Cahlan Sharp <cahlan@gmail.com>
2014-01-21 13:34:01 -0700

    removed functionality for project

--- script.js
+++ script.js
@@ -29,63 +29,23 @@ $(document).ready(function() {
 	}
 	
 	//shuffle the deck
-	deck = _.shuffle(deck);
 	
-	//divide out the cards into two arrays, one for each player
+	
 	var cards_player_1 = [];
 	var cards_player_2 = [];
-	
-	cards_player_1 = deck.splice(0, Math.ceil(deck.length/2));
-	cards_player_2 = deck;
+	//divide out the cards into the two arrays
 	
 	
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
-	function war(card_1, card_2) {
-		if (card_1.number > card_2.number) {
-			return card_1;
-		}
-		else if (card_2.number > card_1.number) {
-			return card_2;
-		}
-		return false;
+	function war() {
 	}
 	
 	
 	//create a play function
+		//compare the cards
+		//give the winner both cards (at end of deck)
 	function play() {
-		var winning_card = war(cards_player_1[0], cards_player_2[0]);
-		var winner;
-		var loser;
-		if (winning_card) {
-			winner = winning_card == cards_player_1[0] ? cards_player_1 : cards_player_2;
-			var loser = winner == cards_player_1 ? cards_player_2 : cards_player_1;
-			var lost_card = loser.shift();
-			winner.push(lost_card);
-			//move card to back
-			winner.push(winner.shift());
-		}
-		else {
-			//tie, get fourth card from here and compare
-			var c1 = cards_player_1[4];
-			var c2 = cards_player_2[4];
-			var tie_card_winner = war(c1, c2);
-			if (!tie_card_winner) {
-				alert("double tie!!");
-				//uh oh, second tie??
-			}
-			else {
-				winner = tie_card_winner == c1 ? cards_player_1 : cards_player_2;
-				loser = winner == cards_player_1 ? cards_player_2 : cards_player_1;
-				var lost_cards = loser.splice(0, 5);
-				for (var i = 0; i<lost_cards.length; i++) {
-					winner.push(lost_cards[i]);	
-				}
-				//move cards to back
-				for (var i = 0; i<=5; i++) {
-					winner.push(winner.shift());
-				}
-			}
-		}
+		
 		//this function (defined below) will continue to the next turn
 		advance();
 	}
