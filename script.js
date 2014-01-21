f6b186d493797c494ac3956fde822b9ee2328389
Cahlan Sharp <cahlan@gmail.com>
2014-01-21 13:26:26 -0700

    modified for jquery rather than bootstrap

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+	//what does this do?
 	function convert_value_to_string(value) {
 		if (value > 10) {
 			switch (value) {
@@ -17,81 +18,93 @@ $(document).ready(function() {
 		return value.toString();
 	}
 
-
-	//Step 2: Card Model
-	var CardModel = Backbone.Model.extend({
-		defaults: {
-			value: 0,
-			suit: '',
-			str_value: ''
-		}
-	});
-
+	//what does this do?
 	var deck = [];
 	var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
 	for (var i = 0; i<suits.length; i++) {
 		var suit = suits[i];
 		for (var j = 0; j<13; j++) {
-			deck.push(new CardModel({
-				value: j+1, 
-				suit: suit,
-				str_value: convert_value_to_string(j+1)
-			}));
+			deck.push({number: j+1, suit: suit});
 		}
 	}
-
-	//Step 1: shuffle the deck
+	
+	//shuffle the deck
 	deck = _.shuffle(deck);
-
-	//Step 3: Card View
-	var CardView = Backbone.View.extend({
-		template: _.template($('#card-template').html()),
-		render: function() {
-			this.$el.html(this.template(this.model.toJSON()));
+	
+	//divide out the cards into two arrays, one for each player
+	var cards_player_1 = [];
+	var cards_player_2 = [];
+	
+	cards_player_1 = deck.splice(0, Math.ceil(deck.length/2));
+	cards_player_2 = deck;
+	
+	
+	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
+	function war(card_1, card_2) {
+		if (card_1.number > card_2.number) {
+			return card_1;
 		}
-	});
-
-	//Step 4
-	var opp_view = new CardView({el: $("#opp-card")});
-	var my_view = new CardView({el: $("#my-card")});
-
-	var my_deck = [];
-	var opp_deck = [];
-
-	//Step 5 divide up cards
-	function deal_cards() {
-		my_deck = deck.splice(0, deck.length/2);
-		opp_deck = deck;
-	};
-	deal_cards();
-
+		else if (card_2.number > card_1.number) {
+			return card_2;
+		}
+		return false;
+	}
+	
+	
+	//create a play function
+	function play() {
+		var winning_card = war(cards_player_1[0], cards_player_2[0]);
+		var winner;
+		var loser;
+		if (winning_card) {
+			winner = winning_card == cards_player_1[0] ? cards_player_1 : cards_player_2;
+			var loser = winner == cards_player_1 ? cards_player_2 : cards_player_1;
+			var lost_card = loser.shift();
+			winner.push(lost_card);
+			//move card to back
+			winner.push(winner.shift());
+		}
+		else {
+			//tie, get fourth card from here and compare
+			var c1 = cards_player_1[4];
+			var c2 = cards_player_2[4];
+			var tie_card_winner = war(c1, c2);
+			if (!tie_card_winner) {
+				alert("double tie!!");
+				//uh oh, second tie??
+			}
+			else {
+				winner = tie_card_winner == c1 ? cards_player_1 : cards_player_2;
+				loser = winner == cards_player_1 ? cards_player_2 : cards_player_1;
+				var lost_cards = loser.splice(0, 5);
+				for (var i = 0; i<lost_cards.length; i++) {
+					winner.push(lost_cards[i]);	
+				}
+				//move cards to back
+				for (var i = 0; i<=5; i++) {
+					winner.push(winner.shift());
+				}
+			}
+		}
+		//this function (defined below) will continue to the next turn
+		advance();
+	}
+	
 	function advance() {
-		opp_view.model = opp_deck[0];
-		my_view.model = my_deck[0];
-		opp_view.render();
-		my_view.render();
-		console.log("my cards", my_deck.length, "opp's cards", opp_deck.length);
+		//take the top two cards and display them
+		if (cards_player_1.length) {
+			var card_1 = cards_player_1[0];
+			var card_2 = cards_player_2[0];
+			$("#opp-card").html(card_1.number+" "+card_1.suit);
+			$("#opp-card-count").html(cards_player_1.length);
+			$("#my-card").html(card_2.number+" "+card_2.suit);
+			$("#my-card-count").html(cards_player_2.length);
+			
+		}
 	}
 	advance();
-
-	//Step 6
-	function war(card1, card2) {
-		return card1.get('value') > card2.get('value') ? card1 : card2;
-	}
-
-	//Step 7
+	
 	$(".btn").click(function() {
-		var my_card = my_deck.shift();
-		var opp_card = opp_deck.shift();
-		var winner = war(my_card, opp_card);
-		if (winner == my_card) {
-			my_deck.push(my_card, opp_card);
-			console.log("you win!");
-		}
-		else if(winner == opp_card) {
-			opp_deck.push(my_card, opp_card);
-			console.log("you lost!");
-		}
-		advance();
-	})
+		play();
+	});
 });
